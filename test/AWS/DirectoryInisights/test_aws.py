b71b39496fe024a69ff136fc8c92bcb116de8c4a
Joe Workman <joe.workman@jumpcloud.com>
2022-04-27 10:28:52 -0600

    generate data & test another service

--- test/AWS/DirectoryInisights/test_aws.py
+++ test/AWS/DirectoryInisights/test_aws.py
@@ -5,6 +5,7 @@ import gzip
 import json
 import subprocess
 import urllib.request
+import requests
 import re
 from re import search
 from pathlib import Path
@@ -14,7 +15,34 @@ def run_subproc(cmd):
     sp = subprocess.run(['python3', cmd], stdout=subprocess.PIPE, text=True)
     return sp.stdout
 
+def generate_data():
+    # Add a system group
+    url = "https://console.jumpcloud.com/api/v2/systemgroups"
+    payload = {"name": "aws_di_app"}
+    headers = {
+        "x-api-key": os.environ['JC_API_KEY'],
+        "content-type": "application/json"
+    }
+    response = requests.request("POST", url, json=payload, headers=headers)
+    jr = json.loads(response.text)
+    # Remove a system group
+    url = "https://console.jumpcloud.com/api/v2/systemgroups/" + jr['id']
+    response = requests.request("DELETE", url, headers=headers)
+    # Add a system group
+    url = "https://console.jumpcloud.com/api/v2/systemgroups"
+    payload = {"name": "aws_di_app"}
+    headers = {
+        "x-api-key": os.environ['JC_API_KEY'],
+        "content-type": "application/json"
+    }
+    response = requests.request("POST", url, json=payload, headers=headers)
+    jr = json.loads(response.text)
+    # Remove a system group
+    url = "https://console.jumpcloud.com/api/v2/systemgroups/" + jr['id']
+    response = requests.request("DELETE", url, headers=headers)
+
 def test_script_produces_output_with_all_services():
+    generate_data()
     pwd = os.path.dirname(os.path.realpath(__file__))
 
     print("running file...")
@@ -40,25 +68,32 @@ def test_json_contents_for_all_services():
         j = json.loads (data.decode('utf-8'))
     # non empty json
     assert len(j) != 0
+    # service can include all types
     for i in j:
-        # print(i['service'])
         assert i['service'] == 'directory' or i['service'] == 'radius' or i['service'] == 'systems' or i['service'] == 'sso' or i['service'] == 'ldap' or i['service'] == 'mdm'
+    j.sort(key = lambda x:x['timestamp'], reverse=True)
+    # data should be sorted correctly
+    assert j[0]['timestamp'] > j[len(j)-1]['timestamp']
+    for file in files:
+        # remove file for next test
+        os.remove(file)
 
-# def test_json_again():
-#     os.environ['incrementType'] = "day"
-#     os.environ['incrementAmount'] = "1"
-#     # os.environ['BucketName'] = 
-#     os.environ['service'] = 'directory'
-#     os.environ['OrgId'] = '5a4bff7ab17d0c9f63bcd277'
-#     pwd = os.getcwd()
-#     print("running file...")
-#     print(pwd + "/get-jcdirectoryinsights.py")
-#     run_subproc(pwd + "/get-jcdirectoryinsights.py")
-#     files = glob.glob("jc_directoryinsights_2022-04-25*.json.gz")
-#     for file in files:
-#         assert os.path.exists(pwd + "/" + file)
-#         print(file)
-#         os.remove(file)
+def test_json_again():
+    os.environ['incrementType'] = "day"
+    os.environ['incrementAmount'] = "1"
+    os.environ['service'] = 'directory'
+    os.environ['OrgId'] = '5ebeb8c7de6f1e713e19cfba'
+    pwd = os.path.dirname(os.path.realpath(__file__))
+    print("running file...")
+    print(pwd + "/get-jcdirectoryinsights.py")
+    run_subproc(pwd + "/get-jcdirectoryinsights.py")
+    files = glob.glob(pwd + "/jc_directoryinsights*.json.gz")
+    for i in j:
+        assert i['service'] == 'directory'
+    for file in files:
+        assert os.path.exists(pwd + "/" + file)
+        # remove file for next test
+        os.remove(file)
 
 def test_changelog_version():
     pwd = os.path.dirname(os.path.realpath(__file__))
